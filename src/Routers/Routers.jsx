a1c737300118f44c7b714ff6c326f428be794bed
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-08-11 23:01:26 +0600

    add notice

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layouts/Main';
 import Course from '../pages/Course/Course';
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
 
 const routers = new createBrowserRouter([
     {
         path: '/',
         element: <Main />,
         children: [
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/register',
+                element: <Register />,
+            },
             {
                 path: '/courses',
                 element: <Course />,
---
+++ src/components/Notice/Notice.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+function Notice() {
+    return (
+        <section className="bg-red-500 text-white text-lg font-semibold py-2 w-full">
+            <Marquee>
+                This website is still under development, it is not usable now, we hope our developer
+                team will make it usable by 20th August 2023
+            </Marquee>
+        </section>
+    );
+}
+
+export default Notice;
